d41943d20bab696e0e21ee0c815d8d6328df9667
Ben Hogan <j.benhogan@gmail.com>
2020-06-18 16:03:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,9 @@
 def reverse_each_word(string)
-  new_array = string.split(" ")
-  new_array.each do |char|
-    
+  array_to_string = string.split(" ")
+  new_array = []
+  
+  array_to_string.each do |char|
+    new_array << char.reverse   
   end
+  new_array.join(" ")
 end
